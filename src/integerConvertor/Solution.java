358a633907589b1188ee163920621932a500e9b2
azmatr4z <robinson.aaron.42@gmail.com>
2019-01-27 17:26:20 +0000

    Starting point for task to show starting time.

---
+++ src/integerConvertor/Solution.java
@@ -0,0 +1,24 @@
+package integerConvertor;
+
+import java.io.IOException;
+
+public class Solution {
+
+	    // Complete the repeatedString function below.
+	    static String integerConvertor(int input) {
+
+	    	return "";
+	    }
+
+
+	    public static void main(String[] args) throws IOException {
+
+	        
+	    	int input = 0;
+	    	
+	        String result = integerConvertor(input);
+
+	        System.out.println("The number " + String.valueOf(input) + " as a Roman Numeral is: " + result);
+	    }
+	
+}
